ba088297a79e7c104b330834eb1e32c632124fa0
Abdul Razzaq <ar087218@gmail.com>
2025-01-07 11:35:25 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Abdul razzaq',
+    author_email= 'ar087218@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
